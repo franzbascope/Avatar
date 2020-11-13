69f589332c10af0fc238a72c5c07f4f62a9aaf54
Franz Bascope <fb12263@infsalesgroup.com>
2020-11-13 08:10:13 -0500

    add icons to display

---
+++ src/components/accesories/iconItem.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Gi3DHammer } from "react-icons/gi";
+
+export default ({ item }) => {
+  return <Gi3DHammer />;
+};
--- src/components/accesories/index.js
+++ src/components/accesories/index.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { accessories } from "./accesories";
 import { Accordion, Card, Button } from "react-bootstrap";
+import { IconContext } from "react-icons";
+import IconItem from "./iconItem";
 
 export default () => {
-  return accessories.map((accesory, iterator) => {
+  return accessories.map((accessory, iterator) => {
     const index = iterator + 1;
     return (
       <Accordion defaultActiveKey={index}>
         <Card>
           <Card.Header>
             <Accordion.Toggle as={Button} variant="link" eventKey={index}>
-              {accesory.name}
+              {accessory.name}
             </Accordion.Toggle>
           </Card.Header>
           <Accordion.Collapse eventKey={index}>
-            <Card.Body>Hello! I'm the body</Card.Body>
+            <Card.Body>
+              {accessory.items.map((item) => {
+                return <IconItem item={item} />;
+              })}
+            </Card.Body>
           </Accordion.Collapse>
         </Card>
       </Accordion>
