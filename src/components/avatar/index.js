aa4d96443523b4bad4e16aa7cd72290d37766f3d
Franz Bascope <fb12263@infsalesgroup.com>
2020-11-27 22:03:03 -0500

    add images

--- src/components/avatar/index.js
+++ src/components/avatar/index.js
@@ -1,18 +1,27 @@
+import React from "react";
 import { bodyConfig } from "../../conf/bodyConfig";
+import { Row, Col } from "react-bootstrap";
 import BodyItem from "./bodyItem";
+import Hat from "../../images/peinados/hair1.jpg";
+import Body from "../../images/body/body1.jpg";
+import Pants from "../../images/bottoms/bottom1.jpg";
 
 export default () => {
-  return bodyConfig.map((mappedItem, index) => {
-    return <BodyItem item={mappedItem} key={index} />;
-    // const ConstructItems = (bodyItem, key) => {
-    //   if (bodyItem.items) {
-    //     return (
-    //       <BodyItem key={key}>{bodyItem.items.map(ConstructItems)}</BodyItem>
-    //     );
-    //   } else {
-    //     return <BodyItem key={key} bodyItem={bodyItem} />;
-    //   }
-    // };
-    // return <BodyItem bodyItem={bodyItem} />;
-  });
+  return (
+    <div>
+      <img src={Hat} style={{ height: "160px" }} />
+      <div
+        style={{
+          marginTop: "20px",
+          marginLeft: "20px",
+          backgroundColor: "#ffdbb5",
+          borderRadius: "100%",
+          width: "140px",
+          height: "110px",
+        }}
+      ></div>
+      <img src={Body} style={{ width: "180px" }} />
+      <img src={Pants} style={{ width: "180px" }} />
+    </div>
+  );
 };
