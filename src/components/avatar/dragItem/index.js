8b26cccc22451947cb017f2c8e78b8e1237b6460
Franz Bascope <fb12263@infsalesgroup.com>
2020-11-28 11:43:56 -0500

    add drag item

--- src/components/avatar/dragItem/index.js
+++ src/components/avatar/dragItem/index.js
@@ -4,19 +4,21 @@ import { useState } from "react";
 export default ({ type, style }) => {
   const [image, setImage] = useState("");
   const [{ isOver }, drop] = useDrop({
-    accept: type ? type : "accessory",
-    drop: (item, monitor) => setImage(item.name),
+    accept: type ? type : "",
+    drop: (item, monitor) => {
+      setImage(item.imageName);
+    },
     collect: (monitor) => ({
       isOver: !!monitor.isOver(),
     }),
   });
-  const parsedImage = image ? image : type + "1";
+  const parsedImage = image ? image : type + "1.jpg";
 
   return (
     <div>
       <img
         ref={drop}
-        src={`${process.env.PUBLIC_URL}/${type}/${parsedImage}.jpg`}
+        src={`${process.env.PUBLIC_URL}/${type}/${parsedImage}`}
         style={style}
       />
     </div>
