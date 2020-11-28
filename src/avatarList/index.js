85cbc7c5feaed345db95ac2891fa47cd242315ff
Franz Bascope <fb12263@infsalesgroup.com>
2020-11-28 18:40:49 -0500

    add style to index page

--- src/avatarList/index.js
+++ src/avatarList/index.js
@@ -1,25 +1,29 @@
 import { useGlobal } from "reactn";
-import { Card } from "react-bootstrap";
+import { Card, Row, Col } from "react-bootstrap";
 import Avatar from "../components/avatar";
 export default () => {
   const [avatars, setAvatars] = useGlobal("avatars");
-  return avatars.map((avatar) => {
-    return (
-      <div className="col-md-4">
-        <Avatar avatar={avatar.items} showAvatarForm={false} height={350}>
-          <Card>
-            <Card.Body>
-              <Card.Title>Author: {avatar.userName}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                {avatar.avatarName}
-              </Card.Subtitle>
-              <Card.Link href="#">Use</Card.Link>
-              <Card.Link href="#">Edit</Card.Link>
-              <Card.Link href="#">Delete</Card.Link>
-            </Card.Body>
-          </Card>
-        </Avatar>
-      </div>
-    );
-  });
+  return (
+    <Row>
+      {avatars.map((avatar) => {
+        return (
+          <Col md="4">
+            <Avatar avatar={avatar.items} showAvatarForm={false} height={350}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Author: {avatar.userName}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {avatar.avatarName}
+                  </Card.Subtitle>
+                  <Card.Link href="#">Use</Card.Link>
+                  <Card.Link href="#">Edit</Card.Link>
+                  <Card.Link href="#">Delete</Card.Link>
+                </Card.Body>
+              </Card>
+            </Avatar>
+          </Col>
+        );
+      })}
+    </Row>
+  );
 };
