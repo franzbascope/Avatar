b7767d0b74be0c74023470c95dfd146be621dfe6
Franz Bascope <fb12263@infsalesgroup.com>
2020-11-29 10:29:40 -0500

    add accessories

--- src/components/accesories/accesories.js
+++ src/components/accesories/accesories.js
@@ -2,7 +2,7 @@ import Types from "../../conf/types";
 export const accessories = [
   {
     name: Types.eyes,
-    items: ["eyes1.png", "eyes2.png"],
+    items: ["eyes1.png", "eyes2.png", "eyes3.png", "eyes4.png", "eyes5.png"],
   },
   {
     name: Types.hairs,
@@ -12,13 +12,17 @@ export const accessories = [
       "hairs3.png",
       "hairs4.png",
       "hairs5.png",
-      "hairs6.png",
-      "hairs7.png",
     ],
   },
   {
     name: Types.mouth,
-    items: ["mouth1.png", "mouth2.png", "mouth3.png", "mouth4.png"],
+    items: [
+      "mouth1.png",
+      "mouth2.png",
+      "mouth3.png",
+      "mouth4.png",
+      "mouth5.png",
+    ],
   },
   {
     name: Types.body,
@@ -26,6 +30,12 @@ export const accessories = [
   },
   {
     name: Types.bottoms,
-    items: ["bottoms1.png", "bottoms2.png", "bottoms3.png"],
+    items: [
+      "bottoms1.png",
+      "bottoms2.png",
+      "bottoms3.png",
+      "bottoms4.png",
+      "bottoms5.png",
+    ],
   },
 ];
